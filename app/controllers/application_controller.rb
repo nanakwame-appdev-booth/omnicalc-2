e004ca12ec172205b504dffa875c9c0e52902212
jasonantwi <jantwi@chicagobooth.edu>
2021-08-05 05:51:59 +0000

    Created answer pages for multiplication, division, & subtraction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,17 +8,23 @@ def submit_add
 end
 
 def submit_subtract
+  @first_num = params.fetch(:first_num).to_f
+  @second_num = params.fetch(:second_num).to_f
 
   render({:template => "results/submit_subtract.html.erb"})
 end
 
 def submit_multiply
+  @first_num = params.fetch(:first_num).to_f
+  @second_num = params.fetch(:second_num).to_f
 
   render({:template => "results/submit_multiply.html.erb"})
 end
 
 def submit_divide
-
+  @first_num = params.fetch(:first_num).to_f
+  @second_num = params.fetch(:second_num).to_f
+  
   render({:template => "results/submit_divide.html.erb"})
 end
 
