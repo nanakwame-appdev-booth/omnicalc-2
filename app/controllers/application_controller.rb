efefe623865b1a84b1f505aa6d08053b60e30c2c
jasonantwi <jantwi@chicagobooth.edu>
2021-08-05 05:31:52 +0000

    RCAV for addition, multiplication, division, and subtraction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
 
+def submit_add
+
+  render({:template => "results/submit_add.html.erb"})
+end
+
+def submit_subtract
+
+  render({:template => "results/submit_subtract.html.erb"})
+end
+
+def submit_multiply
+
+  render({:template => "results/submit_multiply.html.erb"})
+end
+
+def submit_divide
+
+  render({:template => "results/submit_divide.html.erb"})
+end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+get("/wizard_add", {:controller => "application", :action => "submit_add"})
+get("/wizard_subtract", {:controller => "application", :action => "submit_subtract"})
+get("/wizard_multiply", {:controller => "application", :action => "submit_multiply"})
+get("/wizard_divide", {:controller => "application", :action => "submit_divide"})
+
 end
